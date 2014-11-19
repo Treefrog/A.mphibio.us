6c3277cca85b1d7d25828d6f12f7e8c1b0713cb9
Clive Moore <clive@cm3.ca>
2014-11-19 14:54:10 -0500

    Added to project libs folder

--- js/libs/jquery.parallax-1.1.3.js
+++ js/libs/jquery.parallax-1.1.3.js
@@ -6,6 +6,7 @@ Twitter: @IanLunn
 Author URL: http://www.ianlunn.co.uk/
 Plugin URL: http://www.ianlunn.co.uk/plugins/jquery-parallax/
 
+
 Dual licensed under the MIT and GPL licenses:
 http://www.opensource.org/licenses/mit-license.php
 http://www.gnu.org/licenses/gpl.html
